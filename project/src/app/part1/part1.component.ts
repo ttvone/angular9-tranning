fd7bba8c21b6b915fb334f4f7ffbd4aad1446ac5
wanchaloem laoket <wanchaloemlaoket@wanchaloems-MBP.ST-244F>
2020-06-21 11:04:38 +0700

    add validation

--- project/src/app/part1/part1.component.ts
+++ project/src/app/part1/part1.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-part1',
@@ -16,9 +16,18 @@ export class Part1Component implements OnInit {
   constructor(private builder: FormBuilder) {
     /** create default form data */
     this.form = this.builder.group({
-      productName: [],
-      productAmount: [0],
-      productPrice: [0]
+      productName: ['', [
+        Validators.required,
+        Validators.maxLength(10)
+      ]],
+      productAmount: [0, [
+        Validators.required,
+        Validators.min(1)
+      ]],
+      productPrice: [0, [
+        Validators.required,
+        Validators.min(1)
+      ]]
     });
   }
 
@@ -27,6 +36,7 @@ export class Part1Component implements OnInit {
 
   /** for submit form */
   onSubmit() {
+    if (this.form.invalid) return this.form.markAllAsTouched();
     console.log(this.form.value);
   }
 
